7ef71ccbeabaf5605460f659b4cad821c51465e2
karanamkathik <karanamkarthik2002@gmail.com>
2025-02-11 11:52:07 +0530

    Initial commit

---
+++ introduction.js
@@ -0,0 +1,33 @@
+console.log('hello world')
+let age = 30
+console.log(age)
+
+let name = 'karthik';
+console.log(name);
+console.log(name[2]);
+
+let add = '20'
+console.log(++add);
+
+// name[2]="p"
+console.log(name[2])
+
+
+let nums = [1, 2, 3]
+let numbers = nums
+
+console.log(nums == numbers)
+
+let value =[1,2,3]
+let value2=[1,2,3]
+
+console.log(value == value2)
+
+console.log('sum of a and b is')
+
+let a = 3
+let b = 4
+
+console.log(`sum of ${a} and ${b} is ${a+b}`)
+
+
