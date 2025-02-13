c6092a031048c8b673d6d7de7590af068dd73de1
karanamkathik <karanamkarthik2002@gmail.com>
2025-02-13 22:20:00 +0530

    initial commit

--- day 7 - function/Exercises/level1/4to12.js
+++ day 7 - function/Exercises/level1/4to12.js
@@ -29,3 +29,43 @@ function volumeOfRectPrism(length,width,height){
 console.log(volumeOfRectPrism(223,976,265))
 
 console.log(volumeOfRectPrism(223,976))
+
+//7.Area of a circle is calculated as follows: area = π x r x r. Write a function which calculates areaOfCircle
+
+function areaOfCircle(r){
+    return Math.PI*r*r;
+}
+console.log(areaOfCircle(5))
+
+const areaOfCircle1=(radius)=>{
+     return Math.PI*radius*radius;
+}
+console.log(areaOfCircle1(9876))
+
+//8.Circumference of a circle is calculated as follows: circumference = 2πr. Write a function which calculates circumOfCircle
+
+function circumference(r){
+    return 2*Math.PI*r
+}
+
+console.log(circumference(987))
+
+const circumference1=(r)=>{
+    return 2*Math.PI*r
+}
+console.log(circumference1(87))
+
+//9.Density of a substance is calculated as follows:density= mass/volume. Write a function which calculates density.
+
+function density(mass,volume){
+   return mass/volume;
+}
+
+console.log(density(87,87))
+
+const density1=(mass,volume)=>{
+    return mass/volume
+}
+console.log(density1(87,87))
+
+//10.
