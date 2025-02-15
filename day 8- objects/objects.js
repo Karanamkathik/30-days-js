5094a04c881d417d3df9abcaa3ff5b51842b4426
karanamkathik <karanamkarthik2002@gmail.com>
2025-02-15 21:43:30 +0530

    initial commit

--- day 8- objects/objects.js
+++ day 8- objects/objects.js
@@ -20,3 +20,30 @@ console.log(person.Name())
 
 //An object is a mutable data structure and we can modify the content of an object after it gets created.
 
+person.nationaliy = 'india'
+person.college = 'IIIT BH'
+person.hobbies.push('cricket')
+person.hobbies.push('chess')
+ 
+
+console.log(person)
+
+const copyPerson = Object.assign({}, person)
+console.log(copyPerson)
+
+
+//gETTING VALUES---.we get values as arrays
+  const values = Object.values(person)
+  console.log(values)
+
+  //Getting ---Keys of object
+
+  const keys =Object.keys(copyPerson)
+  console.log(keys)
+
+  const keys1 =Object.keys(person.hobbies)
+  console.log(keys1)
+
+
+  const values1=Object.values(person.hobbies)
+  console.log(values1)
