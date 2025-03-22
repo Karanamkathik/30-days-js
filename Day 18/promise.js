e5e26851d7334a9fb2b4265d632e4b9ec84018dd
karanamkathik <karanamkarthik2002@gmail.com>
2025-03-22 19:11:47 +0530

    initial commit

--- Day 18/promise.js
+++ Day 18/promise.js
@@ -7,11 +7,13 @@
       
 */
 
-//promise constructure
-//We can create a promise using the Promise constructor.
-//  We can create a new promise using the key word new followed by the word Promise and followed by a parenthesis.
-//  Inside the parenthesis, it takes a callback function. 
+// promise constructure
+// We can create a promise using the Promise constructor.
+// We can create a new promise using the key word new followed by the word Promise and followed by a parenthesis.
+// Inside the parenthesis, it takes a callback function. 
 // The promise callback function has two parameters which are the resolve and reject functions.
+
+
 const promise = new Promise((resolve, reject) => {
    resolve('sucessful')
    reject('failed')
@@ -55,3 +57,52 @@ fetch(url)
 .then(response => response.json())
 .then(data => console.log(data))
 .catch(error=>console.log(error))
+
+
+// Async/Await = Async = makes a function return a promise
+// Await = makes an async function wait for a promise
+
+// Allows you write asynchronous code in a synchronous manner
+// Async doesn't have resolve or reject set up as parameters
+// Everything after Await is placed in an event queue.
+
+function walkDog(){
+   return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+         const dogWalked = true;
+         if(dogWalked){
+            resolve('dog walked')
+         }else{
+            reject('dog not walked')
+         }
+      },2000)
+   })
+}
+
+function doHouse(){
+   return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+         const housecleaned = false;
+         if(housecleaned){
+            resolve('house cleaned')
+         }else{
+            reject('not cleaned')
+         }
+      },3000)
+   })
+}
+ 
+async function work(){
+   try{
+      const dogWalked = await walkDog()
+      console.log(dogWalked)
+      const houseCleaned = await doHouse()
+      console.log(houseCleaned)
+   }catch(error){
+      console.log('error')
+   }
+}
+
+
+work();
+
